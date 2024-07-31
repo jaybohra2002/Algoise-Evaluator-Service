91e7a97ba10e76e7e34158563d898ba38ad67efc
jaybohra2002 <jaybohra380@gmail.com>
2024-07-31 19:23:53 +0530

    Basic ioredis setup with bullmq installation done

--- src/controllers/pingControllers.ts
+++ src/controllers/pingControllers.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from "express";
 
 export const pingCheck = (_: Request, res: Response) => {
-  
-  
   return res.status(200).json({
     message: "Ping check Ok",
   });
